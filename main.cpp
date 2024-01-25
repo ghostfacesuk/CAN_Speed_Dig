bc55792d982533e368902e8b52805069e176aed3
Steve G <ssghostfacekillah@hotmail.com>
2024-01-25 10:11:57 +0000

    added CAN time prints and LED controls 10s

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "CAN.h"
 
 DigitalOut led1(LED1);  // MBED LED1
+DigitalOut led2(LED2);  // MBED LED4
+DigitalOut led3(LED3);  // MBED LED4
 DigitalOut led4(LED4);  // MBED LED4
 DigitalOut pin21(p21);  // Output for external LED and buzzer
 CAN can1(p9, p10);      // CAN pins, CAN port 1
@@ -23,6 +25,7 @@ int main() {
     bool ledOn = false;
     uint16_t prevCanValue = 0;
     uint32_t canTime = 0;
+    uint32_t ontime = 1;
 
     while (1) {
         if (can1.read(msg)) {
@@ -38,21 +41,38 @@ int main() {
                 uint16_t canValueInteger = canValue;
                 uint16_t canValueDecimal = (float) (canValue - canValueInteger) * 100;
 
-                // Check for transitions from below 60 to above 60 or vice versa
-                if ((canValue < 60 && prevCanValue >= 60) || (canValue >= 60 && prevCanValue < 60)) {
-                    // Convert raw time to HH:MM:SS format
-                    int hours, minutes;
-                    float seconds;
-                    convertTime(canTime, hours, minutes, seconds);
+                 // Convert raw time to HH:MM:SS format
+                int hours, minutes;
+                float seconds;
+                convertTime(canTime, hours, minutes, seconds);
 
                 // Perform the conversion without using floats
                 uint16_t timeInteger = seconds;
                 uint16_t timeDecimal = (float) (seconds - timeInteger) * 100;
 
+                // LED controls using CAN time. This will print once a second and turn on 2 LEDs
+                //if (timeInteger == 45 && timeDecimal == 00) {
+                if (0 == ontime) {
+                    led2 = 0;
+                    led3 = 0;
+                }
+                else ontime--;
+                
+                if (0 == (canTime % (10 * 100))) {
+                    printf("Time: %02u:%02u:%02u.%02u, Speed: %02u.%02u\r\n", hours, minutes, timeInteger, timeDecimal, canValueInteger, canValueDecimal);
+                    led2 = 1;
+                    led3 = 1;
+                    ontime = 10;
+                } else {
+                    //led2 = 0;
+                    //led3 = 0;
+                }
+
+                // Check for transitions from below 60 to above 60 or vice versa
+                if ((canValue < 60 && prevCanValue >= 60) || (canValue >= 60 && prevCanValue < 60)) {
+
                     // Print the time, CAN value, and the transition
                     printf("Time: %02u:%02u:%02u.%02u, Speed: %02u.%02u\r\n", hours, minutes, timeInteger, timeDecimal, canValueInteger, canValueDecimal);
-                    //printf("TEST Speed: %02u.%02u\r\n", canValueInteger, canValueDecimal); 
-                    //printf("TEST Time: %02u.%02u\r\n", timeInteger, timeDecimal);
                 }
 
                 // Check if the received value is greater than or equal to 60
